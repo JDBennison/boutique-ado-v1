5ed562f3c8fedbf8b33eed97d8729be6c1691919
JDBennison <jamesbennison88@gmail.com>
2021-01-18 23:57:49 +0000

    Deployment to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '0)ch(kj_rfb$_8ekoa(0bcou$ynr#0ka3ettv75u6#0vovywq^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bennison-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,16 +122,19 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://lodthirzepizwk:ec2d1933fcedda0c55257930f03209cb8179cf5f18809191be4b5cb5c8927de7@ec2-54-247-122-209.eu-west-1.compute.amazonaws.com:5432/d8ccvimmam812e')
-}
+
 
 
 # Password validation
